874856d5c8aaeeabec257c6ad5fe20e5c7215e3d
AnanasekE <janeksambor@gmail.com>
2022-01-21 15:14:16 +0100

    Added some stuff

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 var field = ['','','','','','','','','']
 //           0  1  2  3  4  5  6  7  8
 
+var wins = [
+    [0,1,2],// poziome
+    [3,4,5],// poziome
+    [6,7,8],// poziome
+    [0,3,6],// pionowe
+    [1,4,7],// pionowe
+    [2,5,8],// pionowe
+    [0,4,8],// skośne
+    [2,4,8],// skośne
+]
+
 var player = 0;
+var player0sign = 'O';
+var player1sign = 'X';
 
 function move(event) {
     var id = event.target.id
+    var elem = document.getElementById(id)
+    
     if (id) {
         
         if (field[id]) {
@@ -12,17 +27,22 @@ function move(event) {
         }
         
         if (player){
-            field[id] = 'O'
+            field[id] = player0sign
+            elem.innerHTML = player0sign
             player = 0
             console.log(field);
             return
         } 
         if (!player) {
-            field[id] = 'X'
+            field[id] = player1sign
+            elem.innerHTML = player1sign
             player = 1
             console.log(field);
             return
         }
         
     }
+}
+function checkWin() {
+    console.log('check win')
 }
